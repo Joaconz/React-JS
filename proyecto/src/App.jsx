7bcfa78941923396806321e00331fde74638204f
Joaquin Nuñez <joaquinunez2004@gmail.com>
2022-05-04 10:32:55 -0300

    Carpeta NavBar

--- proyecto/src/App.jsx
+++ proyecto/src/App.jsx
@@ -1,11 +1,6 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
-
-//import Container from 'react-bootstrap/Container';
-//import Nav from 'react-bootstrap/Nav';
-//import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
--- proyecto/src/components/NavBar/NavBar.jsx
+++ proyecto/src/components/NavBar/NavBar.jsx
@@ -5,20 +5,24 @@ function NavBar() {
     return (
 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">Andre-Cucina</Navbar.Brand>
+  <Navbar.Brand href="#home">Andre Cucina</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="#features">Inicio</Nav.Link>
       <Nav.Link href="#pricing">Sobre nosotros</Nav.Link>
+      <Nav.Link href='#'>Cómo comprar</Nav.Link>
       <NavDropdown title="Delicias" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Tortas</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Galletas</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Panificados</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Sin Tacc</NavDropdown.Item>
       </NavDropdown>
+      <Nav.Link href="#deets">Contacto</Nav.Link>
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">Contacto</Nav.Link>
+      <Nav.Link href="#deets">carrito</Nav.Link>
     </Nav>
   </Navbar.Collapse>
   </Container>
