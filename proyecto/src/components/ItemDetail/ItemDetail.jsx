db66faa9994ee4b1c20b0554be94f97270032d54
Joaquin Nuñez <joaquinunez2004@gmail.com>
2022-06-22 23:39:25 -0300

    modificaciones finales

--- proyecto/src/components/ItemDetail/ItemDetail.jsx
+++ proyecto/src/components/ItemDetail/ItemDetail.jsx
@@ -6,19 +6,12 @@ import LoadingSpinnet from "../../Helpers/LoadingSpinnet";
 import ItemCount from "../ItemCount/ItemCount";
 
 const ItemDetail = ({ product }) => {
-  const [option, setOption] = useState(0);
-
   const cartContext = useContext(CartContext);
-  const { addToCart, selectOptions, optionSelected } = cartContext;
+  const { addToCart } = cartContext;
 
   const onAdd = (cant) => {
     addToCart(product, cant, product.id);
   };
-
-/*let sourceImg = product.image[optionSelected]
-console.log(optionSelected)
-console.log(product.image[optionSelected])*/
-let sourceImg = false
 
   return (
     <div>
@@ -27,20 +20,12 @@ let sourceImg = false
           <section className="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-24 mx-auto">
               <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                {sourceImg?
-              <img
-                  alt="ecommerce"
-                  className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                  src={sourceImg}
-                />
-              :  
-              <img
+                <img
                   alt="ecommerce"
                   className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
                   src={product.image}
                 />
-              }
-                
+                             
                 <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                   <h2 className="text-sm title-font text-gray-500 tracking-widest">
                     Custom Your Desk
@@ -112,56 +97,7 @@ let sourceImg = false
                   <p className="leading-relaxed">{product.description}</p>
 
                   <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                    {product.size === undefined ? (
-                      <div className="flex">
-                        <span className="mr-3">Color</span>
-                        <button
-                          id="white"
-                          className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"
-                          onClick={() => selectOptions(0)}
-                        ></button>
-                        <button
-                          id="black"
-                          className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"
-                          onClick={() => selectOptions(1)}
-                        ></button>
-                        <button
-                          id="blue"
-                          className="border-2 border-gray-300 ml-1 bg-blue-600 rounded-full w-6 h-6 focus:outline-none"
-                          onClick={() => selectOptions(2)}
-                        ></button>
-                        <button
-                          id="violet"
-                          className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"
-                          onClick={() => selectOptions(3)}
-                        ></button>
-                      </div>
-                    ) : (
-                      <div className="flex ml-6 items-center">
-                        <span className="mr-3">Size</span>
-                        <div className="relative">
-                          <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                            <option>SM</option>
-                            <option>M</option>
-                            <option>L</option>
-                            <option>XL</option>
-                          </select>
-                          <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                            <svg
-                              fill="none"
-                              stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              classNameName="w-4 h-4"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M6 9l6 6 6-6"></path>
-                            </svg>
-                          </span>
-                        </div>
-                      </div>
-                    )}
+
                   </div>
                   <span className="title-font font-medium text-2xl text-gray-900">
                     $ {product.price}
